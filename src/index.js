c4b8769f58b16f44e95af896a7b8d193218d0b27
Argonaute 2022 <105778713+Argonaute2022@users.noreply.github.com>
2022-09-12 17:13:56 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const typeorm = require('typeorm');
+const Wilder = require('./entity/Wilder');
+
+const app = express();
+
+const datasource = new typeorm.DataSource({
+  type: 'sqlite',
+  database: './wildersdb.sqlite',
+  synchronize: true,
+  entities: [require("./entity/Wilder")],
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const start = async () => {
+  await datasource.initialize();
+  datasource.getRepository(Wilder).save({ name: "First Wilder"});
+  app.listen(3000, () => console.log('listening on port 3000'));
+
+};
+
+start();c
